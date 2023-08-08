8253120e8a41804b61e4e3879fd4251c32fff388
aalbino221 <aalbino221@gmail.com>
2023-08-08 13:22:33 -0300

    Third commit

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const mongoose = require('mongoose');
 
-var app = express();
+mongoose.set('strictQuery', false);
+const mongoDB = `mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_PASS}@cluster0.nfqh7wx.mongodb.net/message-board?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +36,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ populatedb.js
@@ -0,0 +1,4 @@
+const Message = require('./models/message');
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
